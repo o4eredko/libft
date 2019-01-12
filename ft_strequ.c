e569ae698ec6ccf2164b440bd8f434d09c4dd712
IDEAPAD320\Scur <evgeny.ocheredko@gmail.com>
2019-01-12 21:44:28 +0200

    Working on map validation

--- ft_strequ.c
+++ ft_strequ.c
@@ -17,9 +17,7 @@ int	ft_strequ(char const *s1, char const *s2)
 	i = 0;
 	if (!s1 || !s2)
 		return (0);
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] == s2[i] && s1[i] && s2[i])
 		i++;
-	if (s1[i] != '\0' || s2[i] != '\0')
-		return (0);
-	return (1);
+	return (!s1[i] && !s2[i]);
 }
